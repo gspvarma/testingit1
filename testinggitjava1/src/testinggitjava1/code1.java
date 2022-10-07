6a1208e48e8426adcf4d0f28d934b0ebed5f7219
gspva <gspva@DESKTOP-KIBEURL>
2022-10-07 11:05:42 +0530

    commitchange1

--- testinggitjava1/src/testinggitjava1/code1.java
+++ testinggitjava1/src/testinggitjava1/code1.java
@@ -5,6 +5,7 @@ public class code1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("Hello");
+  System.out.println("Welcome to java programming");
 	}
 
 }
